4b478d39dd9ae3fcfa6185476e051cfc9aafacea
Samlv9 <samlv9@live.com>
2016-02-27 22:22:46 +0800

    1.0

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+var StaticServer = require('node-static').Server;
+var fileServer   = new StaticServer("./wwwroot");
+
+
+http.createServer(function ( request, response ) {
+    request.addListener('end', function () {
+        /// 创建静态文件服务器。
+        fileServer.serve(request, response);
+    })
+    .resume();
+}).listen(port);
